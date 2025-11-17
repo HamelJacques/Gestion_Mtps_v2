a92375d63d4d23f577630c2d1e6a3d1899d4656e
HamelJacques <hamel_jacques@hotmail.com>
2025-11-17 08:38:04 -0500

    Initial commit

---
+++ Gestion_Mtps_v2/frmOuverture.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Gestion_Mtps_v2
+{
+    partial class frmOuverture
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFermer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFermer
+            // 
+            this.btnFermer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFermer.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFermer.Location = new System.Drawing.Point(20, 114);
+            this.btnFermer.Name = "btnFermer";
+            this.btnFermer.Size = new System.Drawing.Size(435, 52);
+            this.btnFermer.TabIndex = 0;
+            this.btnFermer.Text = "Fermer";
+            this.btnFermer.UseVisualStyleBackColor = true;
+            this.btnFermer.Click += new System.EventHandler(this.btnFermer_Click);
+            // 
+            // frmOuverture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 178);
+            this.Controls.Add(this.btnFermer);
+            this.Name = "frmOuverture";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFermer;
+    }
+}
+
---
+++ Gestion_Mtps_v2/frmOuverture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_Mtps_v2
+{
+    public partial class frmOuverture : Form
+    {
+        #region DONNÉES MEMBRES
+        private string m_Titre;
+        #endregion
+        #region CONSTRUCTEUR
+        public frmOuverture()
+        {
+            InitializeComponent();
+            InitForm();
+        }
+        #endregion
+
+        #region MÉTHODES PRIVÉES
+        private void InitForm()
+        {
+            m_Titre = "Ouverture";
+            this.Text = m_Titre;
+        }
+        private void btnFermer_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+
+    }
+}
