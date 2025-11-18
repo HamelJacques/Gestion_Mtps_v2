ebf43edade5300c61d374b9721f4fb4c0345a939
HamelJacques <46819138+HamelJacques@users.noreply.github.com>
2025-11-18 08:40:36 -0500

    Ajout de la classe Logger

---
+++ Gestion_Mtps_v2/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Mtps
+{
+    
+    internal class Logger
+    {
+        #region DONNÉES MEMBRES
+        private string m_message;
+        #endregion
+        #region CONSTRUCTEURS
+        public Logger()
+        {
+            m_message = string.Empty;
+        }
+        public Logger(string lemessage, string cheminEXE)
+        {
+            m_message = lemessage;
+            Log(lemessage, cheminEXE);
+        }
+        #endregion
+        #region MÉTHODES PRIVÉES
+        private void Log(string message, string cheminEXE)
+        {
+            try
+            {
+                string logFile = Path.Combine(cheminEXE, "application.log");
+                string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}";
+                File.AppendAllText(logFile, logEntry + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                // En cas d'erreur de journalisation, on peut ignorer ou afficher un message
+                Debug.WriteLine($"Erreur lors de l'écriture du log : {ex.Message}");
+            }
+        }
+        #endregion
+
+    }
+}
